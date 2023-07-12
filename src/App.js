c302f5cc5fad8de68e6806d2e4210f46726d9f58
Jeremy <jip101@gmail.com>
2023-07-11 21:48:25 -0400

    move usestate hook to board component

--- src/App.js
+++ src/App.js
@@ -3,58 +3,37 @@ import './App.css';
 import { useState } from 'react';
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null))
+
   return (
     <>
       <div className='board-row'>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[0]} />
+        <Square value={squares[1]}/>
+        <Square value={squares[2]}/>
       </div>
       <div className='board-row'>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[3]}/>
+        <Square value={squares[4]}/>
+        <Square value={squares[5]}/>
       </div>
       <div className='board-row'>
-        <Square />
-        <Square />
-        <Square />
+        <Square value={squares[6]}/>
+        <Square value={squares[7]}/>
+        <Square value={squares[8]}/>
       </div>
     </>
   )
 }
 
-function Square() {
-  const [newGame, setGame] = useState(true);
-  const [value, setValue] = useState(true);
-
-  function handleClick() {
-    setValue(!value)
-  }
-
-  function handleOtherClick() {
-    setGame(!newGame)
-  }
-
-  if (newGame !== true) {
-    return (
-      <button 
-        className='square'
-        onClick={handleClick}
-      >
-        {value ? 'X' : 'O'}
-      </button>
-    );
-  }
-  else {
-    return (
-      <button 
-        className='square'
-        onClick={handleOtherClick}
-      >
-      </button>
-    );
-  }
+function Square({ value }) {
+  return (
+    <button 
+      className='square'
+    >
+      {value}
+    </button>
+  );
 }
 
   /*
